8b06481bb4fcf68ad9618bda9fbd264f6d8c6a33
Cosmin-Marius Ungureanu <marius.ungureanu@fortech.ro>
2022-02-12 12:21:59 +0200

    secondary heeader changes

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -9,18 +9,18 @@ import { Card } from 'src/app/models/cardModel';
 export class HomeComponent implements OnInit {
   public cards: Array<Card> = [
     { title: "Initiere",
-      subTitle: "Primi pasi pe tatami",
-      text: "Alaturi de noi, copii dumneavoastra pot face primii pasi intr-o calatorie magica a universului artelor martiale.",
+      subTitle: "Primii pasi pe tatami",
+      text: "Alaturi de noi, copiii dumneavoastra pot face primii pasi intr-o calatorie magica a universului artelor martiale.",
       imagePath: "assets/img/o_goshi.webp"
     },
     { title: "Dezvoltare personala",
       subTitle: "Evolutie",
-      text: "Autocunoasterea prin arta martiala ofera sansa unui suport emotional si psihologic solid pentru viitorul adult.",
+      text: "Educatia este un proces de lunga durata care duce la evolutie, insa nu este niciodata prea tarziu sa mai inveti ceva.",
       imagePath: "assets/img/ipon_seoi_nage.webp"
     },
     { title: "Armonie",
-      subTitle: "Echilibru psiho emotional",
-      text: "Armonia este o consecinta a antrenamentului focalizat martial si se dobandeste prin autocunoastere si experiente multple pe tatami.",
+      subTitle: "Coordonarea dintre minte si corp",
+      text: "Armonia este o consecinta a antrenamentului focalizat martial si se dobandeste prin experiente multiple care duc la autocunoastere.",
       imagePath: "assets/img/uchi_mata.webp"
      }
   ];
