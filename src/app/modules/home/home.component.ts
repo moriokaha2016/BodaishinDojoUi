d11488bc0edf3c51e0feef0bc0f977d48576451d
Cosmin-Marius Ungureanu <marius.ungureanu@fortech.ro>
2022-02-13 12:12:53 +0200

    diacritice pentru text

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -10,17 +10,17 @@ export class HomeComponent implements OnInit {
   public cards: Array<Card> = [
     { title: "Initiere",
       subTitle: "Primii pasi pe tatami",
-      text: "Alaturi de noi, copiii dumneavoastra pot face primii pasi intr-o calatorie magica a universului artelor martiale.",
+      text: "Alături de noi, copiii dumneavoastră pot face primii pași într-o călătorie magică a universului artelor marțiale.",
       imagePath: "assets/img/o_goshi.webp"
     },
     { title: "Dezvoltare personala",
       subTitle: "Evolutie",
-      text: "Educatia este un proces de lunga durata care duce la evolutie, insa nu este niciodata prea tarziu sa mai inveti ceva.",
+      text: "Educatia este un proces de lunga durată care duce la evolutie, insa nu este niciodata prea tarziu sa mai inveti ceva.",
       imagePath: "assets/img/ipon_seoi_nage.webp"
     },
     { title: "Armonie",
       subTitle: "Coordonarea dintre minte si corp",
-      text: "Armonia este o consecinta a antrenamentului focalizat martial si se dobandeste prin experiente multiple care duc la autocunoastere.",
+      text: "Armonia este o consecință a antrenamentului focalizat marțial și se dobândește prin experiențe multiple care duc la autocunoaștere.",
       imagePath: "assets/img/uchi_mata.webp"
      }
   ];
